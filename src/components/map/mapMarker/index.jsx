17dc625fee5258712e26f10325b8d819d4eb8aa4
Sandesh Pandey <sandeshsinbox@gmail.com>
2025-08-07 10:46:49 -0500

    Add stroke option for map markers

--- src/components/map/mapMarker/index.jsx
+++ src/components/map/mapMarker/index.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useCallback, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
 import { useMapbox } from '../../../context/mapContext';
 import './index.css';
-import { ZOOM_LEVEL_MARGIN  } from '../utils/constants';
+import { ZOOM_LEVEL_MARGIN } from '../utils/constants';
 import { getMarkerColor } from '../utils';
 
 
@@ -108,10 +108,13 @@ export const MarkerFeature = ({ vizItems, onClickOnMarker }) => {
  * @param {string} [strokeColor='#000000'] - Optional stroke color.
  * @returns {string} SVG string to be injected into the DOM.
  */
-const getMarkerSVG = (color, strokeColor = '#000000') => {
+const getMarkerSVG = (color, strokeColor = 'rgba(0, 0, 0, 0)') => {
   return `
     <svg fill="${color}" width="30px" height="30px" viewBox="-51.2 -51.2 614.40 614.40" xmlns="http://www.w3.org/2000/svg">
       <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="${strokeColor}" stroke-width="10.24">
+        <path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path>
+      </g>
       <g id="SVGRepo_iconCarrier">
         <path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path>
       </g>
